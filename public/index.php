cc5917963abed70bcfa892d43fda24e5e6a95b85
Massetho <quentin_thomasset@hotmail.fr>
2018-11-27 16:03:33 +0100

    #7 - Blackfire integration

--- public/index.php
+++ public/index.php
@@ -7,6 +7,20 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../vendor/autoload.php';
 
+
+if (isset($_SERVER['HTTP_BLACKFIRETRIGGER'])) {
+    // let's create a client
+    $blackfire = new \Blackfire\Client();
+    // then start the probe
+    $probe = $blackfire->createProbe();
+
+    // When runtime shuts down, let's finish the profiling session
+    register_shutdown_function(function () use ($blackfire, $probe) {
+        // See the PHP SDK documentation for using the $profile object
+        $profile = $blackfire->endProbe($probe);
+    });
+}
+
 // The check is to ensure we don't use .env in production
 if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
     if (!class_exists(Dotenv::class)) {
